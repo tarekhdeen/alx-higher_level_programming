44843a12c0cba89ad0b54f531fff411fa680ec8a
tarekhdeen <tarekhdeen@gmail.com>
2023-11-28 17:07:40 -0800

    1

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 import random
+import math
 number = random.randint(-10000, 10000)
 mod = number % 10 if number > 10 else number % -10
 print(
@@ -10,4 +11,4 @@ if mod > 5:
 elif mod == 0:
     print("0")
 else:
-    print("less than 6 and 0")
+    print("less than 6 and not 0")
