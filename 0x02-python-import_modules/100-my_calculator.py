67c68075ac8d5d6bf2b1fde37d1db36153b2a1de
tarekhdeen <tarekhdeen@gmail.com>
2023-11-30 13:02:49 -0800

    edit 100

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
         print("{} - {} = {}".format(a, b, sub(a, b)))
     elif op == "*":
         print("{} * {} = {}".format(a, b, mul(a, b)))
-    if op == "/":
+    elif op == "/":
         print("{} / {} = {}".format(a, b, div(a, b)))
     else:
         print("Unknown operator. Available operators: +, -, * and /")
