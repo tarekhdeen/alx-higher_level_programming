3be76a897bae55ca8a03b0f5b01fce4e0c7fba5c
tarekhdeen <tarekhdeen@gmail.com>
2023-12-19 16:03:09 -0800

    edit

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -25,11 +25,11 @@ class Square:
 
     @size.setter
     def size(self, value):
-        if not isinstance(size, int):
+        if not isinstance(value, int):
             raise TypeError('size must be an integer')
-        if size < 0:
+        if value < 0:
             raise ValueError('size must be >= 0')
-        self.__size = size
+        self.__size = value
 
     def area(self):
         """Area of this square.
